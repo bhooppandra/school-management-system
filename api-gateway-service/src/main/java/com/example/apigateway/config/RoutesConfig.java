cf2e7f79eb4ae8754365657c93befd3fe6f11daa
Bhooppandra Jat <bhooppandra@gmail.com>
2022-09-03 15:00:30 +0530

    Working codes

--- api-gateway-service/src/main/java/com/example/apigateway/config/RoutesConfig.java
+++ api-gateway-service/src/main/java/com/example/apigateway/config/RoutesConfig.java
@@ -1,32 +1,25 @@
-/*
-package com.example.apigateway.config;
-
-
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
-
-@Component
-@EnableHystrix
-public class RoutesConfig {
-
-    @Bean
-    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route(p -> p
-                        .path("/get")
-                        .filters(f -> f.addRequestHeader("Hello", "World"))
-                        .uri("http://httpbin.org:80"))
-                .route(p -> p
-                        .host("*.circuitbreaker.com")
-                        .filters(f -> f.circuitBreaker(config -> config
-                                .setName("mycmd")
-                                .setFallbackUri("forward:/fallback")))
-                        .uri("http://httpbin.org:80"))
-                .build();
-    }
-}
-*/
+//
+//package com.example.apigateway.config;
+//
+//
+//import org.springframework.cloud.gateway.route.RouteLocator;
+//import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+//import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@EnableHystrix
+//public class RoutesConfig {
+//
+//    @Bean
+//    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route(p -> p
+//                        .path("/users/**")
+//                        .filters(f -> f.addRequestHeader("Hello", "World"))
+//                        .uri("http://httpbin.org:80"))
+//                .build();
+//    }
+//}
+//
